e7fff50cd39aac280a065ec75bd7233ccbc7afe7
LeslyN <lnombertoc@gmail.com>
2018-02-14 00:19:35 -0500

    método fetch completo

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,31 @@
-// document.addEventListener('load', function() {
+/* Variables del método XHR*/ 
 const form = document.getElementById('search-form');
 const searchField = document.getElementById('search-keyword');
 const responseContainer = document.getElementById('response-container');
 let searchedForText;
+  
+/* Variables del método fetch*/ 
+const btn = document.getElementById('search-fetch-form');
+const searchFetchKeyword = document.getElementById('search-fetch-keyword');
+const responseFetchContainer = document.getElementById('response-fetch-container'); 
+let searchedForTextFetch;
+const url = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForTextFetch}&api-key=ad72b2701f9d46ae8838cc299b8c77a1`;
+  
+  
+/* Método XHR */ 
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
+  getNews();
+});
 
-  form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    responseContainer.innerHTML = '';
-    searchedForText = searchField.value;
-    getNews();
-  });
-
-  function getNews() {
-    const articleRequest = new XMLHttpRequest();
-    articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=ad72b2701f9d46ae8838cc299b8c77a1`);
-    articleRequest.onload = addNews;
-    articleRequest.onerror = handleError;
-    articleRequest.send();
+function getNews() {
+  const articleRequest = new XMLHttpRequest();
+  articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=ad72b2701f9d46ae8838cc299b8c77a1`);
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
 }
 
 function handleError() {
@@ -35,39 +44,33 @@ function addNews() {
   li.className = 'articleClass';
   // li.innerHTML = ``;
   li.innerText = snippet;
-
   responseContainer.appendChild(li);
-  // const response = data.response;
-  // console.log(data);
-  // console.log(response);
 }
 
-// });
+/* Método fetch */ 
+btn.addEventListener('submit', function(e){
+  e.preventDefault();
+  responseFetchContainer.innerHTML = '';
+  searchedForTextFetch = searchFetchKeyword.value;
+    fetch(url)
+    .then(function(response){
+      console.log(response);
+      return response.json()
+    }).then(function(data){
+      // debugger;
+      console.log(data.response);
+      const articleFetch = data.response.docs[0];
+      console.log(articleFetch);
+      const titleFetch = articleFetch.headline.main;
+      const snippetFetch = articleFetch.snippet;
 
-const btn = document.querySelector("button");
-const priceSpan = document.querySelector("#price");
- let currency = 'EUR'; 
-const uri = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-btn.addEventListener('click', function(){
-     fetch(uri)
-     .then(function(response){
-         console.log(response);
-         return response.json()
-     }).then(function(data){
-        console.log(data.bpi[currency].rate);
-        priceSpan.innerText = data.bpi[currency].rate + currency ;
-    })
-     .catch(function(error){
-         console.log(error);
-     }) 
-  /*   fetch(url)
-     .then(function(response){
-         console.log(response);
-         return response.json().then(function(data){
-             console.log(data);
-         })
-     })
-     .catch(function(error){
-         console.log(error);
-     }) */
+      let li = document.createElement('li');
+      li.className = 'articleClass';
+      // li.innerHTML = ``;
+      li.innerText = snippetFetch;
+      responseFetchContainer.appendChild(li);
+  })
+  .catch(function(error){
+    console.log(error);
+  }) 
 })
