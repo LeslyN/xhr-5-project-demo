bd4b7d458ebd50b669d6407161ad977b1e5324f4
LeslyN <lnombertoc@gmail.com>
2018-02-13 22:42:35 -0500

    primer intento para método fetch

--- public/js/app.js
+++ public/js/app.js
@@ -44,3 +44,30 @@ function addNews() {
 
 // });
 
+const btn = document.querySelector("button");
+const priceSpan = document.querySelector("#price");
+ let currency = 'EUR'; 
+const uri = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+btn.addEventListener('click', function(){
+     fetch(uri)
+     .then(function(response){
+         console.log(response);
+         return response.json()
+     }).then(function(data){
+        console.log(data.bpi[currency].rate);
+        priceSpan.innerText = data.bpi[currency].rate + currency ;
+    })
+     .catch(function(error){
+         console.log(error);
+     }) 
+  /*   fetch(url)
+     .then(function(response){
+         console.log(response);
+         return response.json().then(function(data){
+             console.log(data);
+         })
+     })
+     .catch(function(error){
+         console.log(error);
+     }) */
+})
